d029c11b4b8a0dbc999946dcbe9518db5a0f2813
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-03-23 10:23:31 -0300

    operadores de comparacion
    operadores de comparacion

---
+++ Leccion 02/pythonProject1/operadores-de-comparacion.py
@@ -0,0 +1,17 @@
+a = 4
+b = 4
+
+# es igual a...
+resultado = a == b
+print(f'resultado == : {resultado}')
+
+# es diferente a...
+resultado = a != b
+print(f'resultado != : {resultado}')
+
+resultado = a > b
+print(f'resultado > : {resultado}')
+
+resultado = a >= b
+print(f'resultado >= : {resultado}')
+
