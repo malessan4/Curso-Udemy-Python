87390defaba4ba813d17c6579d4241b5c24f0961
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-03-23 10:08:59 -0300

    Ejercicio de rectangulo
    Ejercicio de rectangulo
    operadores en python

---
+++ Leccion 02/pythonProject1/ejercicio-rectangulo.py
@@ -0,0 +1,11 @@
+""""
+triple comilla para comentarios multi linea
+
+"""
+
+alto = int(input('Proporciona el alto del rectangulo: '))
+ancho = int(input('Proporciona el ancho del rectangulo: '))
+area = alto * ancho
+print('Area: ', area)
+perimetro = (alto + ancho) * 2
+print('Perimetro: ', perimetro)
