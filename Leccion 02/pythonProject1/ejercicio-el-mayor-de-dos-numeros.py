5327b1f1d3a263e3fd2369d2eb7c231dc5c02667
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-03-23 17:28:21 -0300

    Add files via upload

---
+++ Leccion 02/pythonProject1/ejercicio-el-mayor-de-dos-numeros.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese un número"))
+numero2 = int(input("Ingrese otro número"))
+
+if numero1 > numero2:
+    print(f'El numero {numero1} es mayor a {numero2}')
+else:
+    print(f'El numero {numero2} es mayor a {numero1}')
