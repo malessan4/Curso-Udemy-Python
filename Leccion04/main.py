546b49c237e0abd9b1db95623217fc2076d465e4
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-04-14 19:36:47 -0300

    Ciclo while

---
+++ Leccion04/main.py
@@ -0,0 +1,13 @@
+# condicion = True
+
+# while condicion:
+#     print('Ejecutando ciclo while')
+# else:
+#     print('Fin del ciclo while')
+
+contador = 0
+while contador < 6:
+    print(contador)
+    contador += 1 #estamos incrementando en uno la variable contador cada vez que se haga una vuelta
+else:
+    print('Fin ciclo while')
