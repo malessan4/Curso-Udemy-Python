9e91bd9e1e7262d0c476b41667f12dcb36936052
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-04-14 12:45:01 -0300

    Calcular que estacion es del año

--- Leccion03/main.py
+++ Leccion03/main.py
@@ -24,14 +24,23 @@ else:
 print(f'Número proporcionado : {numero}: {numeroTexto}')
 '''
 
-
+'''
 condicion = True
 
-'''
-if condicion:
-    print('Condición verdadera')
-else:
-    print('Condición falsa')
-'''
 
 print('Condición verdadera') if condicion else print ('Condición falsa')
+'''
+
+mes = int(input('Proporciona mes del año (1-12): '))
+estacion = None
+if mes == 1 or mes == 2 or mes == 12:
+    estacion = 'Verano'
+elif mes == 3 or mes == 4 or mes == 5:
+    estacion = 'Otoño'
+elif mes == 6 or mes == 7 or mes == 8:
+    estacion = 'Invierno'
+elif mes == 9 or mes == 10 or mes == 11:
+    estacion = 'Primavera'
+else:
+    estacion = 'Valor incorrecto'
+print(f'Para el mes {mes} la estacion es: {estacion}')
