d5e3d5b05055b421cd0da531d480e71b36167ec4
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-04-14 12:32:39 -0300

    seguimos  con else if

--- Leccion03/main.py
+++ Leccion03/main.py
@@ -1,8 +1,37 @@
-condicion = 10
+'''condicion = 10
 
 if condicion == True:
     print('condición verdadera')
 elif condicion == False:
     print('condicion falsa')
 else:
-    print('condición no reconocida')
+    print('condición no reconocida')   
+'''
+
+'''
+numero = int(input('Proporciona un valor entre 1 y 3:'))
+numeroTexto = ''
+
+if numero == 1:
+    numeroTexto = 'Número uno'
+elif numero == 2:
+    numeroTexto = 'Número dos'
+elif numero == 3:
+    numeroTexto = 'Número tres'
+else:
+    numeroTexto = 'Valor fuera de rango'
+
+print(f'Número proporcionado : {numero}: {numeroTexto}')
+'''
+
+
+condicion = True
+
+'''
+if condicion:
+    print('Condición verdadera')
+else:
+    print('Condición falsa')
+'''
+
+print('Condición verdadera') if condicion else print ('Condición falsa')
