a2518962a40458d91117b950c3043fedf3d2b445
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-04-15 20:33:59 -0300

    fin de la leccion 4

--- Leccion04/main.py
+++ Leccion04/main.py
@@ -6,8 +6,10 @@
 #     print('Fin del ciclo while')
 
 contador = 0
-while contador < 6:
+maximo = 5
+while contador <= maximo:
     print(contador)
+    #contador = contador + 1
     contador += 1 #estamos incrementando en uno la variable contador cada vez que se haga una vuelta
 else:
     print('Fin ciclo while')
