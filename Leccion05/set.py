766cb835273f0ee79e5049a66fd2134a0a823d1c
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-04-16 11:41:17 -0300

    coleccion tipo set

--- Leccion05/set.py
+++ Leccion05/set.py
@@ -6,3 +6,11 @@ planetas = {'Marte', 'Jupiter', 'Venus'}
 print(planetas)
 print(len(planetas))
 print('Marte' in planetas)
+planetas.add('Tierra')
+print(planetas)
+planetas.remove('Tierra')
+print(planetas)
+planetas.clear()
+print(planetas)
+del planetas
+print(planetas)
