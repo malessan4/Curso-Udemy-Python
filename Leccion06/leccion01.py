ff693e4ffb2a82aff69af15eadb5b3ceace72f49
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-05-25 21:52:59 -0300

    leccion 84

---
+++ Leccion06/leccion01.py
@@ -0,0 +1,27 @@
+"""
+Imprimir numero de 5 a 1 de manera descendente usando funciones recursivas.
+Puede ser cualquier valor positivo, ejemplo, si pasamos el valor de 5, debe imprimir:
+5
+4
+3
+2
+1
+
+En caso de pasar el valor de 3, debe imprimir:
+3
+2
+1
+
+si se pasan valores negativos no imprime nada
+"""
+
+def numerosDescen(numero):
+    if numero >= 1:
+        print(numero)
+        numerosDescen(numero - 1)
+
+num = int(input('ingrese un numero: '))
+numerosDescen(num)
+
+
+    
