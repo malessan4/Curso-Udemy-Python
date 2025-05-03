183373328cac61d2c64f0d736e9fff093ec216bb
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2025-05-03 16:47:42 -0300

    Mundo_PC

--- Leccion12/Raton.py
+++ Leccion12/Raton.py
@@ -1,4 +1,4 @@
-from DispositivoEntrada.py import DispositivoEntrada
+from DispositivoEntrada import DispositivoEntrada
 
 class Raton(DispositivoEntrada):
     contador_ratones = 0
@@ -6,7 +6,10 @@ class Raton(DispositivoEntrada):
     def __init__(self, marca, tipo_entrada):
         Raton.contador_ratones += 1
         self.id_raton = Raton.contador_ratones
-        self.marca = marca
-        self.tipo_entrada = tipo_entrada
+        super().__init__(marca, tipo_entrada)
+        
+    def __str__(self):
+        return (f'ID: {self.id_raton}, Marca: {self.marca},'
+                f' Tipo Entrada {self.tipo_entrada}')
         
     
